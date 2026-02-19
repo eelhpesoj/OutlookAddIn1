ca86bbf3050a77f7ea7b309de0fa22b4422ff029
joseph.lee <joseph.lee@SWOGM0NCD1N>
2026-02-19 19:49:10 +0900

    Add project files.

---
+++ OutlookAddIn1/Ribbon1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OutlookAddIn1
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            // 이동하고 싶은 URL 주소
+            string url = "https://www.google.com";
+
+            try
+            {
+                // 시스템 기본 브라우저로 URL 실행
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("오류가 발생했습니다: " + ex.Message);
+            }
+        }
+    }
+}
